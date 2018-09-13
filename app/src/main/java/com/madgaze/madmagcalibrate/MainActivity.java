6f96bcedd34788264d09ba710c81e8b3437715b9
bleachangel <lsr20031984@163.com>
2018-09-13 13:43:31 +0800

    每隔1s采集一次数据的算法验证，没能成功计算出校准参数。

--- app/src/main/java/com/madgaze/madmagcalibrate/MainActivity.java
+++ app/src/main/java/com/madgaze/madmagcalibrate/MainActivity.java
@@ -9,6 +9,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -44,7 +45,6 @@ public class MainActivity extends Activity implements SensorEventListener {
 	public List<sample> samples = new ArrayList<sample>();//用于记录采样点的值
 
 	public static final int MAX_COORDINATE = 5;
-	private int coordinate_index;
 	public List<sample> mag = new ArrayList<sample>();//用于计算单个点的均值
 	private int mRecordStatus;
 	public static final int RECORD_STARTED = 0;
@@ -52,7 +52,23 @@ public class MainActivity extends Activity implements SensorEventListener {
     public static final int MAX_SAMPLE_COUNT = 13;
 	public native boolean addSample(double x, double y, double z);
 	public native double[] getParams(double radius);
-	
+
+    Handler mHandler = new Handler();
+    Runnable r = new Runnable() {
+
+        @Override
+        public void run() {
+            //do something
+            //每隔1s循环执行run方法
+            synchronized (MainActivity.this) {
+                mRecordStatus = RECORD_STARTED;
+                if (mStartFlag == RECORDING) {
+                    mHandler.postDelayed(this, 1000);
+                }
+            }
+        }
+    };
+
 	private void initSensorService() {
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
@@ -63,7 +79,6 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mStartFlag = INIT;
-		coordinate_index = 0;
 		mRecordStatus = RECORD_STOPPED;
         mTextViewParams = (TextView)findViewById(R.id.textViewParams);
         mCaliStart = (Button)findViewById(R.id.cali_start);
@@ -73,50 +88,43 @@ public class MainActivity extends Activity implements SensorEventListener {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				if (mStartFlag == INIT){
-					mStartFlag = RECORDING;
-                    mCaliStart.setEnabled(false);
-                    synchronized (MainActivity.this) {
+                synchronized (MainActivity.this) {
+                    if (mStartFlag == INIT) {
+                        mStartFlag = RECORDING;
                         mRecordStatus = RECORD_STARTED;
-                    }
-					mCaliStart.setText(R.string.cali_next);
-                    mCaliStart.setEnabled(true);
-				} else if (mStartFlag == STOPPED){
-					int size = samples.size();
-					mCaliStart.setEnabled(false);
+                        mHandler.postDelayed(r, 100);//延时100毫秒
+                        samples.clear();
 
-					Log.d(TAG, "array size : ("+size+")");
-					for(int i = 0; i < size; i++){
-						sample s;
-						s = samples.get(i);
-						addSample(s.m_x,s.m_y,s.m_z);
-						//Log.d(TAG, "mag: ("+s.m_x+"," + s.m_y + "," + s.m_z +")");
-					}
-					
-					double[] params = getParams(13.0);
-					if(params!=null){
-						mTextViewParams.setText("param : ("+params[0] + ","+params[1] + ","+params[2] + "," +params[3] + ","+params[4] + ","+params[5] +")");
-						Log.d(TAG, "param : ("+params[0] + ","+params[1] + ","+params[2] + ","
-							+params[3] + ","+params[4] + ","+params[5] +")"); 
-					}
+                        mCaliStart.setEnabled(false);
+                        mCaliStart.setText(R.string.cali_stop);
+                        mCaliStart.setEnabled(true);
 
-					mCaliStart.setText(R.string.cali_start);
-					mCaliStart.setEnabled(true);
-					mStartFlag = INIT;
-                    samples.clear();
-					Toast.makeText(MainActivity.this, "vector size :"+size, Toast.LENGTH_SHORT).show();
-				} else if (mStartFlag == RECORDING){
-					//samples.clear();
-					mCaliStart.setEnabled(false);
-                    int size = samples.size();
-                    if(size >= MAX_SAMPLE_COUNT){
+                    } else if (mStartFlag == RECORDING) {
                         mStartFlag = STOPPED;
+                        mCaliStart.setEnabled(false);
+                        int size = samples.size();
+
+                        Log.d(TAG, "array size : (" + size + ")");
+                        for (int i = 0; i < size; i++) {
+                            sample s;
+                            s = samples.get(i);
+                            addSample(s.m_x, s.m_y, s.m_z);
+                            //Log.d(TAG, "mag: ("+s.m_x+"," + s.m_y + "," + s.m_z +")");
+                        }
+
+                        double[] params = getParams(13.0);
+                        if (params != null) {
+                            mTextViewParams.setText("param : (" + params[0] + "," + params[1] + "," + params[2] + "," + params[3] + "," + params[4] + "," + params[5] + ")");
+                            Log.d(TAG, "param : (" + params[0] + "," + params[1] + "," + params[2] + ","
+                                    + params[3] + "," + params[4] + "," + params[5] + ")");
+                        }
+
+                        mCaliStart.setText(R.string.cali_start);
+                        mCaliStart.setEnabled(true);
+                        mStartFlag = INIT;
+                        Toast.makeText(MainActivity.this, "vector size :" + size, Toast.LENGTH_SHORT).show();
                     }
-					synchronized (MainActivity.this) {
-						mRecordStatus = RECORD_STARTED;
-					}
-					mCaliStart.setEnabled(true);
-				}
+                }
 			}
         	
         });
@@ -146,7 +154,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
     }
     
     @Override
@@ -168,51 +176,17 @@ public class MainActivity extends Activity implements SensorEventListener {
         if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
             //mMagneticFieldValues = event.values;
             //addSample(mMagneticFieldValues[0],mMagneticFieldValues[1],mMagneticFieldValues[2]);
-        	if(mStartFlag == RECORDING && mRecordStatus == RECORD_STARTED) {
-				if (coordinate_index < MAX_COORDINATE) {
-					sample cord = new sample();
-					cord.m_x = event.values[0];
-					cord.m_y = event.values[1];
-					cord.m_z = event.values[2];
-					mag.add(cord);
-					coordinate_index++;
-				} else {
-					double x, y, z;
-					double av_x, av_y, av_z;
-					int size;
-					x = mag.get(0).m_x;
-					y = mag.get(0).m_y;
-					z = mag.get(0).m_z;
-					if (mag.size() < MAX_COORDINATE) {
-						size = mag.size();
-					} else {
-						size = MAX_COORDINATE;
-					}
-					for (int i = 1; i < mag.size() && i < MAX_COORDINATE; i++) {
-						x += mag.get(i).m_x;
-						y += mag.get(i).m_y;
-						z += mag.get(i).m_z;
-					}
+            synchronized (MainActivity.this) {
+                if(mStartFlag == RECORDING && mRecordStatus == RECORD_STARTED) {
+                    mRecordStatus = RECORD_STOPPED;
+                    sample cord = new sample();
+                    cord.m_x = event.values[0];
+                    cord.m_y = event.values[1];
+                    cord.m_z = event.values[2];
 
-					av_x = x / size;
-					av_y = y / size;
-					av_z = z / size;
-
-					synchronized (MainActivity.this) {
-						String coordinateTxt = "X: " + av_x + ", Y: " + av_y + ", Z: " + av_z;
-						mTextViewParams.setText(coordinateTxt);
-
-						sample record = new sample();
-						record.m_x = av_x;
-						record.m_y = av_y;
-						record.m_z = av_z;
-						samples.add(record);
-
-						mRecordStatus = RECORD_STOPPED;
-					}
-
-                    mag.clear();
-                    coordinate_index = 0;
+					//String coordinateTxt = "X: " + cord.m_x + ", Y: " + cord.m_y + ", Z: " + cord.m_z;
+					//mTextViewParams.setText(coordinateTxt);
+					samples.add(cord);
 				}
 			}
         } 
